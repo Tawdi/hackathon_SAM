f67752eb8f1a6f23ea9e026615ddb6a0e92cf740
tawdi <ahmedtawdi00@gmail.com>
2024-08-02 17:28:25 +0100

    ejs

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,16 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('pages/accueil');
 });
-router.get('/evenements', (req, res) => {
-    res.render('pages/accueil');
+router.get('/propos', (req, res) => {
+    res.render('pages/propos');
+});
+router.get('/contact', (req, res) => {
+    res.render('pages/contact');
+});
+router.get('/actualites', (req, res) => {
+    res.render('pages/actualites');
 });
 router.get('/evenements', (req, res) => {
-    res.render('pages/accueil');
+    res.render('pages/evenements');
 });
 module.exports = router;
