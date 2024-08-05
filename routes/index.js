e670543d0183cc5963694f32a8c3e863f55b8350
ichrakjaifra <ichrak.jaifra@gmail>
2024-08-05 15:16:05 +0100

    ichrak

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,27 @@ router.get('/', (req, res) => {
 });
 router.get('/propos', (req, res) => {
     res.render('pages/propos',{
-        title: "propos"
+        title: "À Propos"
     });
 });
 router.get('/contact', (req, res) => {
     res.render('pages/contact',{
-        title: "contact"
+        title: "Contact"
     });
 });
 router.get('/actualites', (req, res) => {
     res.render('pages/actualites',{
-        title: "actualites"
+        title: "Actualités"
     });
 });
 router.get('/evenements', (req, res) => {
     res.render('pages/evenements',{
-        title: "evenements"
+        title: "Evénements"
     });
 });
 router.get('/connecter', (req, res) => {
     res.render('auth/login',{
-        title: "connecter"
+        title: "Connecter"
     });
 });
 module.exports = router;
