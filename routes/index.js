026c427f45ad686a14a49e9911d9373bcc860be3
tawdi <ahmedtawdi00@gmail.com>
2024-08-05 09:51:26 +0100

    aaaa

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,28 @@ router.get('/', (req, res) => {
     res.render('pages/accueil');
 });
 router.get('/propos', (req, res) => {
-    res.render('pages/propos');
+    res.render('pages/propos',{
+        title: "propos"
+    });
 });
 router.get('/contact', (req, res) => {
-    res.render('pages/contact');
+    res.render('pages/contact',{
+        title: "contact"
+    });
 });
 router.get('/actualites', (req, res) => {
-    res.render('pages/actualites');
+    res.render('pages/actualites',{
+        title: "actualites"
+    });
 });
 router.get('/evenements', (req, res) => {
-    res.render('pages/evenements');
+    res.render('pages/evenements',{
+        title: "evenements"
+    });
 });
 router.get('/connecter', (req, res) => {
-    res.render('auth/login');
+    res.render('auth/login',{
+        title: "connecter"
+    });
 });
 module.exports = router;
