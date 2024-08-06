f766ca061bdbf50f6dc57a7d6c2802ba9c4d272c
imaneelaichi <imaneelaichi224@gmail.com>
2024-08-06 11:05:29 +0100

    imane

--- routes/index.js
+++ routes/index.js
@@ -6,33 +6,33 @@ router.get('/', (req, res) => {
 });
 router.get('/propos', (req, res) => {
     res.render('pages/propos',{
-        title: "propos"
+        title: "A propos"
     });
 });
 router.get('/contact', (req, res) => {
     res.render('pages/contact',{
-        title: "contact"
+        title: "Contact"
     });
 });
 router.get('/actualites', (req, res) => {
     res.render('pages/actualites',{
-        title: "actualites"
+        title: "Actualités"
     });
 });
 router.get('/evenements', (req, res) => {
     res.render('pages/evenements',{
-        title: "evenements"
+        title: "Evénments"
     });
 });
 router.get('/connecter', (req, res) => {
     res.render('auth/login',{
-        title: "connecter"
+        title: "Connecter"
     });
 });
 
 router.get('/detail', (req, res) => {
     res.render('event/details',{
-        title: "connecter"
+        title: "Detail Evénment"
     });
 });
 module.exports = router;
