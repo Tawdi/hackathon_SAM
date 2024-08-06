122af3b44b3573a94ef4e51e4f177f382c22961b
ahmed <ahmedtawdi00@gmail.com>
2024-08-06 19:57:20 +0100

    ahmed

--- config/creation_db.js
+++ config/creation_db.js
@@ -147,10 +147,12 @@ require('dotenv').config();
 // Create a pool of connections
 const pool = mysql.createPool({
 
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-
+  // host: 'localhost',
+  // user: 'root',
+  // password: 'admin',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
 
   // database: 'sys',
   waitForConnections: true,
