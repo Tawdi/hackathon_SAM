528037b225e7e44fa88c494b272ee4a926ad15ec
ichrakjaifra <ichrak.jaifra@gmail>
2024-08-06 13:48:09 +0100

    update

--- config/creation_db.js
+++ config/creation_db.js
@@ -147,7 +147,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'admin',
+  password: 'ichrak',
   // database: 'sys',
   waitForConnections: true,
   connectionLimit: 10,
