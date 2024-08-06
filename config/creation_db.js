559ce8eb2aaa5846db58b7db479e3ed4b34ce95a
tawdi <ahmedtawdi00@gmail.com>
2024-08-06 15:09:13 +0100

    auth

--- config/creation_db.js
+++ config/creation_db.js
@@ -142,12 +142,13 @@
 //config/creation-db.js
 const bcrypt = require("bcrypt");
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Create a pool of connections
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'ichrak',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   // database: 'sys',
   waitForConnections: true,
   connectionLimit: 10,
