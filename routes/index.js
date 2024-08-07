8ef9139f837341a1ad884b57f8d1d2068e0b9e23
imaneelaichi <imaneelaichi224@gmail.com>
2024-08-07 12:04:49 +0100

    imane

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const news = [
+
+    { img: 'img/Image1.jpg', speaker: 'Alice Martin', date: '2024-08-05', title: 'Nouvelle Politique Agricole' },
+    { img: 'img/Image2.jpg', speaker: 'Bob Dupont', date: '2024-07-30', title: 'Techniques Modernes de Culture' },
+    { img: 'img/Image3.jpg', speaker: 'Clara Moreau', date: '2024-07-25', title: 'Innovation dans l’Agroalimentaire' },
+];
+
+const events = [
+    { img: 'img/Image1.jpg', title: 'Salon de l’Agriculture', date: '2024-09-15', location: 'Paris' },
+    { img: 'img/Image2.jpg', title: 'Journée de l’Environnement', date: '2024-10-10', location: 'Lyon' },
+    { img: 'img/Image3.jpg', title: 'Forum de l’Innovation', date: '2024-11-20', location: 'Marseille' },
+];
+
+const sponsors = [
+    { img: 'img/OIP-removebg-preview.png' },
+    { img: 'img/OCP_Logo.png' },
+    { img: 'img/logo-removebg-preview.png' },
+    // Ajoutez plus de sponsors si nécessaire
+];
+
 router.get('/', (req, res) => {
-    res.render('pages/accueil');
+    res.render('pages/accueil',{
+        events,news,sponsors
+    });
+
 });
 router.get('/propos', (req, res) => {
     res.render('pages/propos',{
@@ -49,3 +72,6 @@ router.get('/detail', (req, res) => {
 // });
 
 module.exports = router;
+
+
+
